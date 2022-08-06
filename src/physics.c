19ecff117fb2449a5dee7d620818fe5538f61a63
Knot Hulk <knothulk@mi-d3.msk.21-school.ru>
2022-08-06 17:52:31 +0300

    physics done

--- src/physics.c
+++ src/physics.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
+#define POCK1 20
+#define POCK2 60
+#define ROOF 27
+#define FLOOR 1
 
-void movement(int &ball_x, int &ball_y, int rocket1, int rocket2, int &ball_speedx, int ball_speedy) {
-    
+void movement(int *ball_x, int *ball_y, int rocket1, int rocket2, int *ball_speedx, int *ball_speedy, int *score_left, int *score_right) {
+    *ball_x += (*ball_speedx);
+    *ball_y += (*ball_speedy);
+    if (*ball_x < POCK1) { //выход за поле (за левую ракетку)
+        (*score_right)++;
+        *ball_x = 40;
+        *ball_y = 12;
+        *ball_speedy = -(*ball_speedy); //можно вернуть и ракетки
+        *ball_speedx = -(*ball_speedx);
+        return;
+    }
+    if (*ball_x > POCK2) { //выход за правую ракетку
+        (*score_left)++;
+        *ball_x = 40;
+        *ball_y = 12;
+        *ball_speedy = -(*ball_speedy);
+        *ball_speedx = -(*ball_speedx);
+        return;
+    }
+    if ((*ball_x == POCK1)  && (*ball_y == rocket1 || *ball_y == (rocket1 + 1) || *ball_y == (rocket1 - 1))) { //попадание в первую ракетку
+        *ball_speedx = - (*ball_speedx);
+        *ball_x += (*ball_speedx);
+        *ball_y += (*ball_speedy);
+        return;
+    }
+    if ((*ball_x == POCK2)  && (*ball_y == rocket2 || *ball_y == (rocket2 + 1) || *ball_y == (rocket2 - 1))) { //попадание во вторую
+        *ball_speedx = - (*ball_speedx);
+        *ball_x += (*ball_speedx);
+        *ball_y += (*ball_speedy);
+        return;
+    }
+    if (*ball_y == FLOOR || *ball_y == ROOF) { //пол и потолок
+        *ball_speedy = - (*ball_speedy);
+        *ball_x += (*ball_speedx);
+        *ball_y += (*ball_speedy);
+        return;
+    }
+}
+
+int main() {
+    int ball_x = 59, ball_y = 17, rocket1 = 18, rocket2 = 18, ball_speedx = 1, ball_speedy = 1, score_left = 0, score_right = 0;
+    movement(&ball_x, &ball_y, rocket1, rocket2, &ball_speedx, &ball_speedy, &score_left, &score_right);
+    printf("%d %d %d %d %d %d %d %d", ball_x, ball_y, rocket1, rocket2,  ball_speedx, ball_speedy, score_left, score_right);
+    return 0;
 }
