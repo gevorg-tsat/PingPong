dca1a8ea51f84fca7ab6db3accb34f52ff1ec29a
Despero Schauer <desperos@mi-d2.msk.21-school.ru>
2022-08-07 17:36:31 +0300

    Отрисовка с очищением экрана и заданием его размеров, прошел проверку стиля

--- src/print.c
+++ src/print.c
@@ -4,46 +4,99 @@
 #define FIELD_SIZE_Y 27  // Размер поля по вертикали 25 + 2 стенки
 #define RACKET_LEFT_X 2  // Положение по Х ЛЕВОЙ ракетки
 #define RACKET_RIGHT_X 75  // Положение по Х ПРАВОЙ ракетки
-#define SCORE_LEFT 17  // Крайнее левое положение счета
-#define SCORE_INT 2  // Интервал между цифрами
+#define SCORE_INT_1 2  // Интервал внутренних цифр счета
+#define SCORE_INT_2 4  // Интервал внешних цифр счета
+#define CENTER_SCORE 9  // Коэфицент центровки счета
+#define CENTER_SCORE 9  // Коэфицент центровки счета
 
-int print(int ball_x, int ball_y, int racket_left_y, int racket_right_y, int score_1, int score_2, int score_3, int score_4) {
-  score_1 = (score_1 == 0) ? 11 : score_1;
-  score_2 = (score_2 == 0) ? 11 : score_2;
-  score_3 = (score_3 == 0) ? 11 : score_3;
-  score_4 = (score_4 == 0) ? 11 : score_4;
+int print(int win,  int ball_x, int ball_y, int racket_left_y,
+int racket_right_y, int score_1, int score_2, int score_3,
+int score_4);  // Прототип фунцкии отрисовки
+int score(int cifra);  // Прототип фунцкии счетчика отчков
+
+// Функция отрисовки
+int print(int win, int ball_x, int ball_y, int racket_left_y,
+int racket_right_y, int score_1, int score_2, int score_3,
+int score_4) {
+  printf("\033[0d\033[2J");  // Отчистка экрана
+  printf("\e[8;$35;$82;t");  // Задание размеров экрана
+  printf("\n");
+
+  // Перевод входящих цифр под формат Switch Case
+  score_1 = (score_1 == 0) ? 11 : score_1;  // Пересчет нуля для Switch
+  score_2 = (score_2 == 0) ? 11 : score_2;  // Пересчет нуля для Switch
+  score_3 = (score_3 == 0) ? 11 : score_3;  // Пересчет нуля для Switch
+  score_4 = (score_4 == 0) ? 11 : score_4;  // Пересчет нуля для Switch
+
+  // Вывод табло со счетом
   for (int score_y = 1; score_y <= 5; score_y++) {  // Цикл по оси Y для счета
-    for (int score_x = 1; score_x <= FIELD_SIZE_X - 20; score_x++) {  // Цикл по оси Х для счета
-      (score_x == FIELD_SIZE_X / 2 - 9 - 2) ? score(score_1 * 10 + score_y) : 
-      (score_x == FIELD_SIZE_X / 2 - 9 - 1) ? score(score_2 * 10 + score_y) : 
-      (score_x == FIELD_SIZE_X / 2 - 9) ? printf(":") : 
-      (score_x == FIELD_SIZE_X / 2 - 9 + 1) ? score(score_3 * 10 + score_y) : 
-      (score_x == FIELD_SIZE_X / 2 - 9 + 2) ? score(score_4 * 10 + score_y) : 
-      printf(" ");
+    for (int score_x = 1; score_x <= FIELD_SIZE_X - 20;
+    score_x++) {  // Цикл по оси Х для счета
+      (score_x == FIELD_SIZE_X / 2 - CENTER_SCORE - SCORE_INT_2)  // Цифра 1
+      ? score(score_1 * 10 + score_y)  // Вывод цифры 1 и умножение для CASE
+      : (score_x == FIELD_SIZE_X / 2 - CENTER_SCORE - SCORE_INT_1)  // Цифра 2
+      ? score(score_2 * 10 + score_y)  // Вывод цифры 2 и умножение для CASE
+      : (score_x == FIELD_SIZE_X / 2 - CENTER_SCORE) ? printf(":")  // Делитель
+      : (score_x == FIELD_SIZE_X / 2 - CENTER_SCORE + SCORE_INT_1)  // Цифра 3
+      ? score(score_3 * 10 + score_y)  // Вывод цифры 3 и умножение для CASE
+      : (score_x == FIELD_SIZE_X / 2 - CENTER_SCORE + SCORE_INT_2)  // Цифра 4
+      ? score(score_4 * 10 + score_y)  // Вывод цифры 4 и умножение для CASE
+      : printf(" ");  // Вывод пустоты в счетчике очков
     }
     printf("\n");
   }
-  printf("\n");
-  for (int field_y = 1; field_y <= FIELD_SIZE_Y; field_y++) {  // Цикл по оси Y
-    for (int field_x = 1; field_x <= FIELD_SIZE_X; field_x++) {  // Цикл по оси Х
-      (ball_x == field_x && ball_y == field_y) ?   printf("@") : // Положение мяча
+
+  // Отображение победителя игры. 1 - левый, 2 - правый, deffault - пустота
+  switch (win) {  // CASE для выбора победителя
+  case 0:  // Победил левый игрок
+    printf("\n");
+    for (int x = 1; x < FIELD_SIZE_X / 2 - 8; x++) {
+      printf(" ");  // Вывод пустоты до нужного положения фразы
+    }
+    printf("Player LEFT WIN!\n");  // Победил правый игрок
+    break;
+  case 1:  // Победил правый игрок
+    printf("\n");
+    for (int x = 1; x < FIELD_SIZE_X / 2 - 8; x++) {
+      printf(" ");  // Вывод пустоты до нужного положения фразы
+    }
+    printf("Player RIGHT WIN\n");  // Победил правый игрок
+    break;
+  default:
+    printf("\n");  // Победителя нет
+    break;
+  }
+
+  // Отобрадения поля игры, его границ, мяча и ракеток
+  for (int field_y = 1; field_y <= FIELD_SIZE_Y;
+  field_y++) {  // Цикл по оси Х
+    for (int field_x = 1; field_x <= FIELD_SIZE_X;
+    field_x++) {  // Цикл по оси Х
+      (ball_x == field_x && ball_y == field_y)
+      ? printf("@") :  // Положение мяча
       (field_x == 1 && field_y == 1) ||  // Верхний левый угол
-      (field_x == FIELD_SIZE_X && field_y == 1) ||  // Верхний правый угол\xe2\x99\xa7
+      (field_x == FIELD_SIZE_X && field_y == 1) ||  // Верхний правый угол
       (field_x == 1 && field_y == FIELD_SIZE_Y) ||  // Нижний левый угол
-      (field_x == FIELD_SIZE_X && field_y == FIELD_SIZE_Y) ?  // Нижний правый угол
-      printf("╬") :  // Отрисовка углов
-      (field_y == FIELD_SIZE_Y) ||  //Нижняя стенка
+      (field_x == FIELD_SIZE_X && field_y == FIELD_SIZE_Y)  // Нижний правый у
+      ? printf("╬") :  // Отрисовка углов
+      (field_y == FIELD_SIZE_Y) ||  // Нижняя стенка
       (field_y == 1) ?  // Верхняя стенка
       printf("-") :  // Отрисовка верхней и нижней стенок
       (field_x == 1) ||  // Левая стенка
       (field_x == (FIELD_SIZE_X - 1) / 2) ||
       (field_x == FIELD_SIZE_X) ||  // Праввая стенка
-      (RACKET_LEFT_X == field_x && racket_left_y == field_y - 1) || // Положение верхнего края левой ракетки
-      (RACKET_LEFT_X == field_x && racket_left_y == field_y) || // Среднее положение левой ракетки
-      (RACKET_LEFT_X == field_x && racket_left_y == field_y + 1) || // Положение нижнего края левой ракетки
-      (RACKET_RIGHT_X == field_x && racket_right_y == field_y - 1) || // Положение верхнего края правой ракетки
-      (RACKET_RIGHT_X == field_x && racket_right_y == field_y) || // Среднее положение правой ракетки
-      (RACKET_RIGHT_X == field_x && racket_right_y == field_y + 1) ? // Положение нижнего края правой ракетки
+      // Положение верхнего края левой ракетки
+      (RACKET_LEFT_X == field_x && racket_left_y == field_y - 1) ||
+      // Среднее положение левой ракетки
+      (RACKET_LEFT_X == field_x && racket_left_y == field_y) ||
+      // Положение нижнего края левой ракетки
+      (RACKET_LEFT_X == field_x && racket_left_y == field_y + 1) ||
+      // Положение верхнего края правой ракетки
+      (RACKET_RIGHT_X == field_x && racket_right_y == field_y - 1) ||
+      // Среднее положение правой ракетки
+      (RACKET_RIGHT_X == field_x && racket_right_y == field_y) ||
+      // Положение нижнего края правой ракетки
+      (RACKET_RIGHT_X == field_x && racket_right_y == field_y + 1) ?
       printf("|") :  // Отрисовка левой и правой стенок
       printf(" ");  // Отрисовка пустого поля
     }
@@ -51,157 +104,169 @@ int print(int ball_x, int ball_y, int racket_left_y, int racket_right_y, int sco
   }
   return 0;
 }
+
+// Набор цифр для счетчика очков построчно, 5 строк Х, 8 строк У
 int score(int cifra) {
   switch (cifra) {
+  // ********************* 0
   case 111:
-    printf("|---|");
+    printf("#####");
     break;
   case 112:
-    printf("|   |");
+    printf("#   #");
     break;
   case 113:
-    printf("|   |");
+    printf("#   #");
     break;
   case 114:
-    printf("|   |");
+    printf("#   #");
     break;
   case 115:
-    printf("|---|");
+    printf("#####");
     break;
+  // ********************* 1
   case 11:
-    printf("   /|");
+    printf("    #");
     break;
   case 12:
-    printf("  / |");
+    printf("    #");
     break;
   case 13:
-    printf("    |");
+    printf("    #");
     break;
   case 14:
-    printf("    |");
+    printf("    #");
     break;
   case 15:
-    printf("    |");
+    printf("    #");
     break;
+  // ********************* 2
   case 21:
-    printf("----|");
+    printf("#####");
     break;
   case 22:
-    printf("    |");
+    printf("    #");
     break;
   case 23:
-    printf("-----");
+    printf("#####");
     break;
   case 24:
-    printf("|    ");
+    printf("#    ");
     break;
   case 25:
-    printf("|----");
+    printf("#####");
     break;
+  // ********************* 3
   case 31:
-    printf("----|");
+    printf("#####");
     break;
   case 32:
-    printf("    |");
+    printf("    #");
     break;
   case 33:
-    printf("|---|");
+    printf("#####");
     break;
   case 34:
-    printf("    |");
+    printf("    #");
     break;
   case 35:
-    printf("----|");
+    printf("#####");
     break;
+  // ********************* 4
   case 41:
-    printf("|   |");
+    printf("#   #");
     break;
   case 42:
-    printf("|   |");
+    printf("#   #");
     break;
   case 43:
-    printf("|---|");
+    printf("#####");
     break;
   case 44:
-    printf("    |");
+    printf("    #");
     break;
   case 45:
-    printf("    |");
+    printf("    #");
     break;
+  // ********************* 5
   case 51:
-    printf("|----");
+    printf("#####");
     break;
   case 52:
-    printf("|    ");
+    printf("#    ");
     break;
   case 53:
-    printf("|---|");
+    printf("#####");
     break;
   case 54:
-    printf("    |");
+    printf("    #");
     break;
   case 55:
-    printf("----|");
+    printf("#####");
     break;
+  // ********************* 6
   case 61:
-    printf("-----");
+    printf("#####");
     break;
   case 62:
-    printf("|    ");
+    printf("#    ");
     break;
   case 63:
-    printf("|---|");
+    printf("#####");
     break;
   case 64:
-    printf("|   |");
+    printf("#   #");
     break;
   case 65:
-    printf("|---|n");
+    printf("#####");
     break;
+  // ********************* 7
   case 71:
-    printf("|---|");
+    printf("#####");
     break;
   case 72:
-    printf("    |");
+    printf("    #");
     break;
   case 73:
-    printf("    |");
+    printf("    #");
     break;
   case 74:
-    printf("    |");
+    printf("    #");
     break;
   case 75:
-    printf("    |");
+    printf("    #");
     break;
+  // ********************* 8
   case 81:
-    printf("|---|");
+    printf("#####");
     break;
   case 82:
-    printf("|   |");
+    printf("#   #");
     break;
   case 83:
-    printf("|---|");
+    printf("#####");
     break;
   case 84:
-    printf("|   |");
+    printf("#   #");
     break;
   case 85:
-    printf("|---|n");
+    printf("#####");
     break;
+  // ********************* 9
   case 91:
-    printf("|---|");
+    printf("#####");
     break;
   case 92:
-    printf("|   |");
+    printf("#   #");
     break;
   case 93:
-    printf("|---|");
+    printf("#####");
     break;
   case 94:
-    printf("    |");
+    printf("    #");
     break;
   case 95:
-    printf("----|");
+    printf("#####");
     break;
   }
   return 0;
