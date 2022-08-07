78e424d2c36d7033d8560ea709aeb4c2a684f266
Knot Hulk <knothulk@mi-f4.msk.21-school.ru>
2022-08-07 17:52:30 +0300

    s

--- src/physics.c
+++ src/physics.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define POCK1 6
+#define POCK1 2
 #define POCK2 75
 #define ROOF 25
 #define FLOOR 1
@@ -32,12 +32,12 @@ void movement(int *ball_x, int *ball_y, int rocket1, int rocket2, int *ball_spee
     if ((*ball_x == POCK2)  && (*ball_y == rocket2 || *ball_y == (rocket2 + 1) || *ball_y == (rocket2 - 1))) { //попадание во вторую
         *ball_speedx = - (*ball_speedx);
         *ball_x += (*ball_speedx);
-        *ball_y += (*ball_speedy);
+        *ball_x += (*ball_speedx);
         return;
     }
     if (*ball_y == FLOOR || *ball_y == ROOF) { //пол и потолок
         *ball_speedy = - (*ball_speedy);
-        *ball_x += (*ball_speedx);
+        *ball_y += (*ball_speedy);
         *ball_y += (*ball_speedy);
         return;
     }
